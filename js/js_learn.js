b36ad33fa6bb805a69d3f24631c3fda6f434cac3
Your Name <you@example.com>
2023-10-08 12:39:53 +0530

    1st commit from Javascript leaning 8 oct 23

---
+++ js/js_learn.js
@@ -0,0 +1,10 @@
+function findFibonacci(n){
+	let fib = [0,1]
+for(let i = 0; i>=n; i++){
+fib[i] = i;
+// console.log(i);
+}
+console.log(fib.length)
+}
+console.log("running..." + findFibonacci(4));
+
