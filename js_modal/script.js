079971a376ef73ea3f4d3d64491b35cf5083309c
Your Name <you@example.com>
2023-10-22 18:36:29 +0530

    Js modal window completed

--- js_modal/script.js
+++ js_modal/script.js
@@ -1,1 +1,26 @@
 `USE STRICT`;
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const openBtn = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < openBtn.length; i++) {
+  openBtn[i].addEventListener('click', openModal);
+  closeBtn.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  });
+}
