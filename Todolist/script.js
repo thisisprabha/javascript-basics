6a73f2252134fdf8987471f4ea3548564f2e1903
Your Name <you@example.com>
2023-10-27 09:44:21 +0530

    Slightly modified

--- Todolist/script.js
+++ Todolist/script.js
@@ -1,1 +1,26 @@
 `USE STRICT`;
+
+// declaration
+const newTodo = document.querySelector('.todo-list');
+const addedTodo = document.querySelector('.added-todo');
+let todoDB = [];
+let listLi = document.createElement('h1');
+listLi.innerHTML = 'Hello';
+document.body.appendChild(listLi);
+
+//Adding new toto
+document.querySelector('.add-todo').addEventListener('click', function () {
+  newTodo.value;
+  addedTodo.textContent = newTodo.value;
+  todoDB.push(addedTodo.textContent);
+  todoDBf();
+});
+
+function todoDBf() {
+  for (let i = 0; i < todoDB.length; i++) {
+    listLi.innerHTML = todoDB[i];
+    console.log(todoDB.length);
+  }
+}
+
+//Removing todo list
