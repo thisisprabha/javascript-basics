f1747b4f0c2af3684b0d5dd61e76e6218a8fc36c
Your Name <you@example.com>
2023-10-27 19:51:04 +0530

    Just did a decalration

--- Todolist/script.js
+++ Todolist/script.js
@@ -3,24 +3,12 @@
 // declaration
 const newTodo = document.querySelector('.todo-list');
 const addedTodo = document.querySelector('.added-todo');
-let todoDB = [];
-let listLi = document.createElement('h1');
-listLi.innerHTML = 'Hello';
-document.body.appendChild(listLi);
 
 //Adding new toto
 document.querySelector('.add-todo').addEventListener('click', function () {
   newTodo.value;
   addedTodo.textContent = newTodo.value;
-  todoDB.push(addedTodo.textContent);
-  todoDBf();
+  //todoDB.push(addedTodo.textContent);
 });
-
-function todoDBf() {
-  for (let i = 0; i < todoDB.length; i++) {
-    listLi.innerHTML = todoDB[i];
-    console.log(todoDB.length);
-  }
-}
 
 //Removing todo list
