8cb3e6be62eae09a158deb65e60369fc8969385b
thisisprabha <prabhakaran.r91@gmail.com>
2024-02-11 12:57:18 +0530

    Added optional chianing and looping obj,keys & entries

--- behind-the-scene/for_of_loop.js
+++ behind-the-scene/for_of_loop.js
@@ -44,6 +44,37 @@ const restaurant = {
 };
 
 const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
-for (const allitems of menu) console.log(allitems);
+// for (const allitems of menu) console.log(allitems);
 
-for (const allitems of menu.entries()) console.log(`${allitems[0] + 1} : ${allitems[1]}`);
+// for (const allitems of menu.entries()) console.log(`${allitems[0] + 1} : ${allitems[1]}`);
+
+if (restaurant.openingHours.mon) {
+  // console.log(restaurant.openingHours.mon.open);
+}
+
+//with optionalk chaining
+// console.log(restaurant?.openingHours?.mon?.open);
+
+const days = [`sat`, `sun`, `mon`, `tue`, `wed`, `thur`, `fri`];
+for (const day of days) {
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? `closed`;
+  console.log(`On ${day}, We open at ${open}`);
+}
+
+console.log(restaurant?.order?.(0, 1) ?? `no order yet`);
+console.log(restaurant?.orderRissoto?.(0, 1) ?? `no order yet`);
+
+// lOOPING OBJEST KEYS, VALUES and ENTRIES
+const properties = Object.keys(restaurant.openingHours);
+let openStr = `we open on ${properties.length} days : `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+const values = Object.values(restaurant.openingHours);
+console.log(values);
+
+const entry = Object.entries(restaurant.openingHours);
+console.log(entry);
