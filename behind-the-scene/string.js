ab155c222434ca3eeacfa59dd73ba309e3adfe4c
Your Name <you@example.com>
2024-07-29 06:38:49 +0530

    string part 2

--- behind-the-scene/string.js
+++ behind-the-scene/string.js
@@ -2,13 +2,24 @@
 
 const actual_String = 'Hello USA & India people. We love you!';
 const lower_Case = actual_String.toLowerCase();
-console.log(lower_Case);
-console.log(actual_String[0]);
-console.log('Hello'[1]);
-console.log(lower_Case.indexOf('H'));
-console.log(lower_Case.indexOf('h'));
-console.log(lower_Case.lastIndexOf('e'));
-console.log(lower_Case.indexOf('usa'));
-console.log(lower_Case.includes('india'));
-console.log(lower_Case.slice(6, 9));
-console.log(lower_Case.slice(12, 17));
+const UPPER_CASE = actual_String.toUpperCase();
+// console.log(lower_Case);
+// console.log(UPPER_CASE);
+// console.log(actual_String[0]);
+// console.log('Hello'[1]);
+// console.log(lower_Case.indexOf('H'));
+// console.log(lower_Case.indexOf('h'));
+// console.log(lower_Case.lastIndexOf('e'));
+// console.log(lower_Case.indexOf('usa'));
+// console.log(lower_Case.includes('india'));
+// console.log(lower_Case.slice(6, 9));
+// console.log(lower_Case.slice(12, 17));
+
+// Fix Capitalisation
+
+const person_Name = function (name) {
+  console.log(name[0].toUpperCase() + name.slice(1).toLowerCase());
+};
+
+person_Name(`prABHA`);
+person_Name(`Jhonny Bravo`);
