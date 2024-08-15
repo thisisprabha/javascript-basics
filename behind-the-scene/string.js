2639fa3bcee44ccdcec9dd8d425857bebe637f76
Your Name <you@example.com>
2024-08-15 20:33:01 +0530

    string 3rd class partially added 15August

--- behind-the-scene/string.js
+++ behind-the-scene/string.js
@@ -3,23 +3,26 @@
 const actual_String = 'Hello USA & India people. We love you!';
 const lower_Case = actual_String.toLowerCase();
 const UPPER_CASE = actual_String.toUpperCase();
-// console.log(lower_Case);
-// console.log(UPPER_CASE);
-// console.log(actual_String[0]);
-// console.log('Hello'[1]);
-// console.log(lower_Case.indexOf('H'));
-// console.log(lower_Case.indexOf('h'));
-// console.log(lower_Case.lastIndexOf('e'));
-// console.log(lower_Case.indexOf('usa'));
-// console.log(lower_Case.includes('india'));
-// console.log(lower_Case.slice(6, 9));
-// console.log(lower_Case.slice(12, 17));
+console.log(lower_Case);
+console.log(UPPER_CASE);
+console.log(actual_String[0]);
+console.log('Hello'[1]);
+console.log(lower_Case.indexOf('H'));
+console.log(lower_Case.indexOf('h'));
+console.log(lower_Case.lastIndexOf('e'));
+console.log(lower_Case.indexOf('usa'));
+console.log(lower_Case.includes('india'));
+console.log(lower_Case.slice(6, 9));
+console.log(lower_Case.slice(12, 17));
 
 // Fix Capitalisation
-
 const person_Name = function (name) {
   console.log(name[0].toUpperCase() + name.slice(1).toLowerCase());
 };
-
 person_Name(`prABHA`);
 person_Name(`Jhonny Bravo`);
+
+//split method
+console.log(`all +is +well`.split(`+`));
+const [firstName, lastName] = `William Buthher`.split(' ');
+console.log(`Mr. ` + firstName, lastName.toUpperCase());
