94f4dc50b4aba9cdfa30e39bc2a8d15029863ff0
Your Name <you@example.com>
2024-08-26 10:29:59 +0530

    WIP intent card number

--- behind-the-scene/string.js
+++ behind-the-scene/string.js
@@ -32,10 +32,36 @@ function capitalizeName(name) {
   let names = name.split(` `);
   let finalName = [];
   for (let n of names) {
-    finalName.push(n[0].toUpperCase() + n.slice(1).toLowerCase());
-    // finalName.join();
+    finalName.push(n[0].toUpperCase() + n.slice(1).toLowerCase()); // Actual way
+    // finalName.push(n.replace(n[0], n[0].toUpperCase())); // another way
   }
   console.log(finalName.join(` `));
 }
 capitalizeName(`mr. prabha karan`);
 capitalizeName(`jhon lenon lord`);
+
+//paddaing
+console.log(`hello`.padStart(23, `+`).padEnd(46, `+`));
+
+//maskign credit card numbers
+const creditCardNumber = function (num) {
+  const str = num + ' ';
+  const last4Digit = str.slice(-5);
+  // console.log(str);
+  return last4Digit.padStart(str.length, `*`);
+  // return last4Digit;
+};
+console.log(creditCardNumber(4343343443431212));
+console.log(creditCardNumber(782145650));
+
+//intent card number
+
+const cardNumber = function (num) {
+  const str = num + '';
+  console.log(str);
+  const newstr = str.concat('*');
+  console.log(newstr);
+};
+
+console.log(cardNumber(5674982278561243));
+// console.log(`1234567890`.split(5));
